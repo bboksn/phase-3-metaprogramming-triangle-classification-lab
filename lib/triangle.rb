6e78e0f1e319c6c418a613e0ef03fe38be6d428c
Ben <bboksner@LAPTOP-0U0LFUE0.localdomain>
2022-10-14 14:45:02 -0400

    commit tree 11a0c2721f8ebcf2e62d7d1de0ec5db23c02f260

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,46 @@
 class Triangle
   # write code here
+  attr_accessor :a1,:a2,:a3
+ 
+  def initialize(a1,a2,a3)
+    @a1=a1
+    @a2=a2
+    @a3=a3
+ end
+ def kind 
+ valid?
+ case 
+ when @a1 == @a2 && @a1 == @a3
+  :equilateral
+ when  @a1 == @a2 || @a1 == @a3 || @a2 == @a3
+  :isosceles
+ when @a1 !=@a2 || @a1 !=@a3 || @a2 != @a3
+  :scalene
+ end
+
+end
+class TriangleError < StandardError
+end
+
+
+def valid? 
+raise TriangleError unless (@a1==0 ||@a2==0 ||@a3 ==0) &&(@a1 + @a2 >a3 || @a2 +@a3 > @a1 || @a1 +@a3 >@a2)
+end
+
+
+=begin
+
+def valid?
+  begin  
+  if @a1==0 ||@a2==0 ||@a3 ==0
+     raise TriangleError
+  else if @a1 + @a2 >a3 || @a2 +@a3 > @a1 || @a1 +@a3 >@a2
+    raise TriangleError 
+  end
+end
+rescue TriangleError => error 
+  puts error.message
+end
+end
+=end
 end
